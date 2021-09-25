bf3746c286e0c49704cdc544dd1cc4cdbcf9a773
Daniel Corin <dcorin6@gmail.com>
2021-09-25 14:01:23 -0700

    Fix build

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { test } from '../../utils/books'
+import { test } from '../../utils/book_club'
 
 export default async function handler(req, res) {
   const result = await test()
